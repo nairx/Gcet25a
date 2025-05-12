1257a2442ac49086f6e1ce90255f2e96f4238dd8
nairxprv@gmail.com <praveannair@gmail.com>
2025-05-12 20:32:53 +0530

    Updated

---
+++ temp15/script.js
@@ -0,0 +1,42 @@
+dvTitle.innerHTML = "Hello World";
+let a = 10;
+let b = 14;
+let c = 5;
+if (a > b && a > c) {
+  console.log("a is greatest");
+} else if (b > a && b > c) {
+  console.log("b is greatest");
+} else {
+  console.log("c is greatest");
+}
+
+////while
+let i = 1;
+while (i < 4) {
+  console.log(i);
+  i = i + 1;
+}
+///for loop
+for (let j = 1; j < 4; j++) {
+  console.log(j);
+}
+
+//break
+// let z = 0;
+// while (z < 4) {
+//   console.log(z);
+//   z = z + 1;
+//   if (z === 2) {
+//     break;
+//   }
+// }
+
+//continue
+let z = 0;
+while (z < 4) {
+  z = z + 1;
+  if (z === 2) {
+    continue;
+  }
+  console.log(z);
+}
---
+++ temp16/script.js
@@ -0,0 +1,13 @@
+function sayHello(){
+    console.log("Hello World")
+}
+
+function greet(name){
+console.log(name)
+}
+
+function handleClick(){
+    let name = document.getElementById("user").value
+    // console.log("Hello " + name)
+    console.log(`Hello ${name}`)
+}
