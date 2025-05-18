62ae947d353b1b32cf698be5193e75af3e96ffe1
nairxprv@gmail.com <praveannair@gmail.com>
2025-05-18 20:36:01 +0530

    Update

--- temp30/script.js
+++ temp30/script.js
@@ -4,29 +4,38 @@ const products = [
   { id: 3, name: "p3", price: 75 },
 ];
 
-const cart = {}
+const cart = {};
 
 const addToCart = (id) => {
-//insert id and quantity as 1 ie {1:1} if add button
-// on box 1 is clicked, then if add button on box 3 is clicked then
-//cart object should be {1:1,3:1}
-
-}
+  //insert id and quantity as 1 ie {1:1} if add button
+  // on box 1 is clicked, then if add button on box 3 is clicked then
+  //cart object should be {1:1,3:1}
+  // write code here....
+  cart[id] = 1;
+  console.log(cart);
+};
 
 const dispCart = () => {
-  root.innerHTML = "Cart Page"
-}
+  let str = "<h2>Cart</h2>";
+  products.map((value) => {
+    cart[value.id] &&
+      (str += `<div>
+      ${value.name}-${value.price}-<button>-</button>${cart[value.id]}<button>+</button>-${value.price*cart[value.id]}
+     </div>
+    `);
+  });
+  root.innerHTML = str;
+};
 
 const showProducts = () => {
-  let str = ""
-  products.map(value=>{
-    str += 
-    `<div>
+  let str = "";
+  products.map((value) => {
+    str += `<div>
       <h3>${value.name}</h3>
       <h4>${value.price}</h4>
       <button onclick='addToCart(${value.id})'>Add to Cart</button>
      </div>
-    `
-  })
-  root.innerHTML = "<div class='row'>" + str + "</div>"
+    `;
+  });
+  root.innerHTML = "<div class='row'>" + str + "</div>";
 };
