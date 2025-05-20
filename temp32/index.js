ce1debba38c3477a2478d85aaedc6007b954b826
nairxprv@gmail.com <praveannair@gmail.com>
2025-05-20 19:09:17 +0530

    Updated

--- temp32/index.js
+++ temp32/index.js
@@ -1,4 +1,47 @@
 const customers = [
-  { cart: "1234567890", pin: "1234", name: "John", balance: 0 },
-  { cart: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
+  { card: "1234567890", pin: "1234", name: "John", balance: 0 },
+  { card: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
 ];
+
+const validateUser = () => {
+  let card = document.getElementById("card").value;
+  let pin = document.getElementById("pin").value;
+  const found = customers.find(
+    (value) => value.card == card && value.pin == pin
+  );
+  console.log(found);
+  if (found) {
+    let str = `
+      <h2>Welcome ${found.name}</h2>
+      <p>
+        <select id='transaction'>
+          <option>Deposit</option>
+          <option>Withdraw</option>
+        </select>
+      </p>
+      <p>
+        <input type='text' placeholder='Enter Amount'>
+      </p>
+      <p>
+        <button>Submit</button>
+      </p>
+      <hr>
+      <strong>Current Balance:${found.balance}</strong>
+      `;
+    root.innerHTML = str
+  } else {
+    error.innerHTML = "Invalid Card or Pin";
+  }
+};
+
+const login = () => {
+  let str = `
+    <p><span id='error'/></p>
+    <p><input type="text" id="card" placeholder="Card #"></p>
+    <p><input type="password" id="pin" placeholder="4-digit pin"></p>
+    <p><button onclick='validateUser()'>Login</button>
+  `;
+  root.innerHTML = str;
+};
+
+login();
